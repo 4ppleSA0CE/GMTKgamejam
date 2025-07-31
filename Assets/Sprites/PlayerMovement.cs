68edf207886dc59804e7e2d0913311b76bdc43e6
Owen <owenzeng315@gmail.com>
2025-07-31 18:22:33 -0400

    Add player animations and update movement system

--- Assets/Sprites/PlayerMovement.cs
+++ Assets/Sprites/PlayerMovement.cs
@@ -1,22 +1,47 @@
+using TMPro;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
     public float speed;
     public Rigidbody2D body;
+
+    [SerializeField] private SpriteRenderer spriteRenderer;
+
+    private float xPosLastFrame;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
+    {
+        MovementControl();
+
+        FlipCharacterX();
+    }
+
+    private void MovementControl()
     {
         float xInput = Input.GetAxis("Horizontal");
         float yInput = Input.GetAxis("Vertical");
 
         Vector2 direction = new Vector2(xInput, yInput).normalized;
         body.linearVelocity = direction * speed;
+    }
+
+    private void FlipCharacterX()
+    {
+        if (transform.position.x > xPosLastFrame)
+        {
+            spriteRenderer.flipX = false;
+        }
+        else if (transform.position.x < xPosLastFrame)
+        {
+            spriteRenderer.flipX = true;
+        }
+        xPosLastFrame = transform.position.x; 
     }
 }
