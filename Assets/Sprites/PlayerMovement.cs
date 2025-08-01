87c4e2daf0f62248f82c37d0b07d66ca599f12bc
4DerekWang <derekwang0919@gmail.com>
2025-07-31 20:05:59 -0400

    update sprite animations and movement

--- Assets/Sprites/PlayerMovement.cs
+++ Assets/Sprites/PlayerMovement.cs
@@ -7,19 +7,29 @@ public class PlayerMovement : MonoBehaviour
     public Rigidbody2D body;
 
     [SerializeField] private SpriteRenderer spriteRenderer;
+    
+    // Add sprite references for idle and running
+    [SerializeField] private Sprite idleSprite;
+    [SerializeField] private Sprite runningSprite;
 
     private float xPosLastFrame;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private bool isMoving = false;
+    
+    // Start is called before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        // Set initial sprite to idle
+        if (idleSprite != null)
+        {
+            spriteRenderer.sprite = idleSprite;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
         MovementControl();
-
+        UpdateSprite();
         FlipCharacterX();
     }
 
@@ -30,6 +40,21 @@ public class PlayerMovement : MonoBehaviour
 
         Vector2 direction = new Vector2(xInput, yInput).normalized;
         body.linearVelocity = direction * speed;
+        
+        // Check if player is moving
+        isMoving = direction.magnitude > 0.1f;
+    }
+    
+    private void UpdateSprite()
+    {
+        if (isMoving && runningSprite != null)
+        {
+            spriteRenderer.sprite = runningSprite;
+        }
+        else if (!isMoving && idleSprite != null)
+        {
+            spriteRenderer.sprite = idleSprite;
+        }
     }
 
     private void FlipCharacterX()
