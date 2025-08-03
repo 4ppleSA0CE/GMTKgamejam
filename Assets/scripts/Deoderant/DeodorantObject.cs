56a1eb93e62cc7e728ea21bd1ed993c91e00fe55
JeffreyZhuang <jeffreyzhuangyt@gmail.com>
2025-08-03 05:22:28 -0400

    Pernamently show deodorant inventory UI

---
+++ Assets/scripts/Deoderant/DeodorantObject.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class DeodorantObject : MonoBehaviour
+{
+    private static DeodorantObject instance;
+
+    void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(gameObject);
+        }
+
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GlobalInventoryManager.Instance != null)
+        {
+            gameObject.SetActive(true);
+        }
+        else
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
