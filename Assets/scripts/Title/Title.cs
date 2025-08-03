05928bcc530766481835ff3ef5219e618a2cbaf1
JeffreyZhuang <jeffreyzhuangyt@gmail.com>
2025-08-03 03:06:04 -0400

    Finish title

--- Assets/scripts/Title/Title.cs
+++ Assets/scripts/Title/Title.cs
@@ -1,16 +1,22 @@
 using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
 
 public class Title : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown("space"))
+        {
+            Debug.Log("Space was pressesd");
+            SceneManager.LoadScene("Tutorial");
+        }
     }
 }
